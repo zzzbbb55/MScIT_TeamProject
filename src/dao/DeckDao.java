8aa9268eaa8efdfb0f0b910f852bf993c95ea825
坤 杨 <zhujiyangkun@163.com>
2020-02-01 14:36:03 +0000

    Text Dao commit

--- src/dao/DeckDao.java
+++ src/dao/DeckDao.java
@@ -2,6 +2,11 @@ package dao;
 
 import model.Deck;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public interface DeckDao {
     public Deck readDeck();
+    public void close() throws IOException;
+    public void initialize() throws FileNotFoundException, IOException;
 }
