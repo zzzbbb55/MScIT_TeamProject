f4af0c32215a7794aa8fbd8bc881bf364a573971
坤 杨 <zhujiyangkun@163.com>
2020-02-09 14:54:25 +0000

    Rework all

--- test/DaoTest.java
+++ test/DaoTest.java
@@ -1,17 +1,27 @@
-import dao.DeckDao;
 import dao.DeckTextDao;
-import model.Deck;
+import dao.Statistc;
+import dao.StatisticSQLDao;
+import game.Deck;
 
 public class DaoTest {
     public static void main(String[] args){
-        DeckDao dao = new DeckTextDao("StarCitizenDeck.txt");
-        Deck deck;
+        DeckTextDao dao = new DeckTextDao("StarCitizenDeck.txt");
+        Deck deck = new Deck();
         try{
             dao.initialize();
-            deck = dao.readDeck();
-            deck.print();
+            deck = (Deck) dao.readDeck(deck);
         }catch(Exception e){
             e.printStackTrace();
         }
+
+        StatisticSQLDao dao1 = new StatisticSQLDao("org.postgresql.Driver","jdbc:postgresql://localhost:5432/TopTrumps","postgres","postgres");
+        dao1.initialize();
+        Statistc statistc = dao1.readStatistic();
+        dao1.close();
+        System.out.println(statistc.getOverallPlayed());
+        System.out.println(statistc.getCompWons());
+        System.out.println(statistc.getHumanWons());
+        System.out.println(statistc.getAvgDraws());
+        System.out.println(statistc.getMaxRounds());
     }
 }
